e9bb268f8820eb476a79dadcc6723bb9ba42d951
adsvolnei <ads.volneiaurelio@gmail.com>
2026-03-13 23:25:11 -0300

    Traduzindo sistema para PT-BR e ajustando nomes do Admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Familia
 
+# Mantém o registro da sua tabela (não apague isso!)
 admin.site.register(Familia)
+
+# Adiciona as configurações de nome logo abaixo
+admin.site.site_header = "Administração"
+admin.site.site_title = "Administração"
+admin.site.index_title = "Painel de Controle"
